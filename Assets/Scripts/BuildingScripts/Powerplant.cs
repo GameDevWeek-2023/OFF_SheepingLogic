f32a901a67ffbfe5bed7aff1a02f03a83f9f6c00
F2abianski <120093905+F2abianski@users.noreply.github.com>
2023-03-16 20:55:11 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/GameDevWeek-2023/OFF_SheepingLogic""
    This reverts commit aa3494a9acff7aba823130f8f916716a2addd746.

---
+++ Assets/Scripts/BuildingScripts/Powerplant.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerplant : Spawner
+{
+
+    [SerializeField] GameObject wolle;
+
+    public AudioClip building_SFX;
+    private AudioSource audio_src;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        GameObject gob = collision.gameObject;
+        gob.SetActive(false);
+
+        audio_src = GetComponent<AudioSource>();
+        audio_src.PlayOneShot(building_SFX);
+
+        StartCoroutine(SpawnNext(gob));
+    }
+
+
+    IEnumerator SpawnNext(GameObject gob)
+    {
+
+        yield return new WaitForSeconds(spawn_delay);
+
+        gob.SetActive(true);
+        Spawn(gob, gob.transform.forward);
+        Spawn(wolle, -transform.right);
+        Destroy(gob);
+
+    }
+
+}
