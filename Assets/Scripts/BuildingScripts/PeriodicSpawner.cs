5a0ae4248bf287fa1035e5355a0abbd6607a0627
F2abianski <120093905+F2abianski@users.noreply.github.com>
2023-03-16 20:55:58 +0100

    Added Animation

--- Assets/Scripts/BuildingScripts/PeriodicSpawner.cs
+++ Assets/Scripts/BuildingScripts/PeriodicSpawner.cs
@@ -9,11 +9,13 @@ public class PeriodicSpawner : Spawner
     float last_spawn_time;
     
     public GameObject spawn_object;
+    [SerializeField] Animator animator;
     
     // Start is called before the first frame update
     void Start()
     {
         last_spawn_time = Time.time;
+        animator.Play("PlaceObject");
     }
 
     // Update is called once per frame
@@ -25,7 +27,7 @@ public class PeriodicSpawner : Spawner
         if (t - last_spawn_time > spawn_delay)
         {
             last_spawn_time = t;
-
+            animator.Play("SpawnObject");
             Spawn(spawn_object, gameObject.transform.forward);
 
         }
