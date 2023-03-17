1174f086d53d8d57234826ab985d785a756a8809
F2abianski <120093905+F2abianski@users.noreply.github.com>
2023-03-17 14:09:01 +0100

    SpinMaschine funktioniert jetzt

---
+++ Assets/Scripts/spinMaschineScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinMaschineScript : Spawner
+{
+    [SerializeField] GameObject garn;
+    [SerializeField] Animator animator;
+
+    public AudioClip building_SFX;
+    private AudioSource audio_src;
+
+    private void Start()
+    {
+        animator.Play("PlaceObject");
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+
+
+
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.transform.forward ==transform.forward || collision.transform.forward == -transform.forward && collision.name.Contains("Wolle")) 
+        {
+            GameObject gob = collision.gameObject;
+            gob.SetActive(false);
+
+            audio_src = GetComponent<AudioSource>();
+            audio_src.PlayOneShot(building_SFX);
+            animator.Play("ReciveObject");
+
+            StartCoroutine(SpawnNext(gob));
+        }
+    }
+
+
+    IEnumerator SpawnNext(GameObject gob)
+    {
+
+        yield return new WaitForSeconds(spawn_delay);
+        animator.Play("SpawnObject");
+        gob.SetActive(true);
+        Spawn(garn, gob.transform.forward);
+        Destroy(gob);
+    }
+}
