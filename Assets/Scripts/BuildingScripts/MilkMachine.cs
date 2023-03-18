1712e5bbfbb13ce9ad5a64095c234e76845d7f76
F2abianski <120093905+F2abianski@users.noreply.github.com>
2023-03-18 17:47:52 +0100

    paar kleine Änderungen

--- Assets/Scripts/BuildingScripts/MilkMachine.cs
+++ Assets/Scripts/BuildingScripts/MilkMachine.cs
@@ -41,7 +41,7 @@ public class MilkMachine : Spawner
         Spawn(gob, gob.transform.forward);
 
         // Todo schwarze Wolle
-        if (gob.tag == "weissesSchaf" || gob.tag == "schwarzesSchaf")   Spawn(wolle, -transform.right);
+        if (gob.gameObject.tag == "weissesSchaf" || gob.gameObject.tag == "schwarzesSchaf")   Spawn(wolle, -transform.right);
         
         Destroy(gob);
 
